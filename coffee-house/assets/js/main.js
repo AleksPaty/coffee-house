81b64e9dacb317983df100a31fe5d497fe6e403c
Try <alekcandr.patalieta@gmail.com>
2023-12-02 23:00:36 +0200

    fix: turn off hover on mobile

--- coffee-house/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-// --------------------------- burger menu ---------------------------------// 
-
-let burgerBtn = document.querySelector('.header-menu__burger');
-let menuArea = document.querySelector('.header-menu');
-
-burgerBtn.onclick = (e) => {
-    e.preventDefault()
-    menuArea.classList.toggle('header-menu-open')
-}
