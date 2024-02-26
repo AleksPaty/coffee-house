cdf51a6486225f899a9abbed013062529bfe366a
Try <alekcandr.patalieta@gmail.com>
2024-02-26 22:00:34 +0200

    refactor: refactor js to ts

--- news-api/src/components/controller/loader.ts
+++ news-api/src/components/controller/loader.ts
@@ -1,19 +1,24 @@
+type CallbackFunc<T> = (data: T) => void;
+
 class Loader {
-    constructor(baseLink, options) {
+    baseLink: string;
+    options: object;
+
+    constructor(baseLink: string, options: { apiKey: number }) {
         this.baseLink = baseLink;
         this.options = options;
     }
 
     getResp(
-        { endpoint, options = {} },
-        callback = () => {
+        { endpoint, options = {} }: { endpoint: string; options: object },
+        callback = (): void => {
             console.error('No callback for GET response');
         }
     ) {
         this.load('GET', endpoint, callback, options);
     }
 
-    errorHandler(res) {
+    errorHandler(res: Response): Response {
         if (!res.ok) {
             if (res.status === 401 || res.status === 404)
                 console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
@@ -23,18 +28,18 @@ class Loader {
         return res;
     }
 
-    makeUrl(options, endpoint) {
+    makeUrl(options: object, endpoint: string) {
         const urlOptions = { ...this.options, ...options };
         let url = `${this.baseLink}${endpoint}?`;
 
         Object.keys(urlOptions).forEach((key) => {
-            url += `${key}=${urlOptions[key]}&`;
+            url += `${key}=${urlOptions[key as keyof typeof urlOptions]}&`;
         });
 
         return url.slice(0, -1);
     }
 
-    load(method, endpoint, callback, options = {}) {
+    load<T>(method: string, endpoint: string, callback: CallbackFunc<T>, options = {}) {
         fetch(this.makeUrl(options, endpoint), { method })
             .then(this.errorHandler)
             .then((res) => res.json())
